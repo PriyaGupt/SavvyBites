95b9f098108a9f7d02134563c4ad76824f317e4c
Priya Gupta <97391443+PriyaGupt@users.noreply.github.com>
2023-12-30 19:05:20 +0530

    frontened

---
+++ src/data/dashboardlinks.js
@@ -0,0 +1,57 @@
+export const sidebarLinks = [
+    {
+      id: 1,
+      name: "My Profile",
+      path: "/dashboard/my-profile",
+      icon: "VscAccount",
+    },
+    {
+      id: 2,
+      name: "Complaints",
+      path: "/dashboard/all-open-complaints",
+      icon: "VscDashboard",
+    },
+    {
+      id: 3,
+      name: "My complains",
+      path: "/dashboard/my-complaints",
+      type: "Student",
+      icon: "VscVm",
+    },
+    {
+        id: 4,
+        name: "My Resolved complains",
+        path: "/dashboard/my-complaints",
+        type: "Professor",
+        icon: "VscVm",
+      },
+      {
+        id: 5,
+        name: "My Resolved complains",
+        path: "/dashboard/my-complaints",
+        type: "ChiefWarden",
+        icon: "VscVm",
+      },
+    {
+      id: 6,
+      name: "Register Complaints",
+      path: "/dashboard/register-complain",
+      type: "Student",
+      icon: "VscAdd",
+    },
+    {
+      id: 7,
+      name: "MenuList",
+      path: "/dashboard/show-menulist",
+      icon: "VscMortarBoard",
+    },
+    {
+      id: 8,
+      name: "Meal Records",
+      path: "/dashboard/mealRecords",
+      type: "Accountant",
+      icon: "VscVm"
+    }
+    
+  ];
+  
